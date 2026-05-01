a925ba28479b9c0212316822554fdcaa589e263a
yogeshkavhar007 <yogeshkavhar007@gmail.com>
2026-05-01 20:31:23 +0530

    Update 4.2.2.py

--- 4.2.2.py
+++ 4.2.2.py
@@ -21,19 +21,3 @@ print(f"Best selling product: {best_product}")
 print(f"Total quantity sold: {highest_quantity}")
 
 
-# import pandas as pd
-
-# # Prompt the user for the file name
-# file_name = input()
-
-# # Load the data
-# df = pd.read_csv(file_name)
-
-
-# # Find the product with the highest total quantity sold
-# best_product =
-# highest_quantity = 
-
-# # Display the result
-# print(f"Best selling product: {best_product}")
-# print(f"Total quantity sold: {highest_quantity}")
